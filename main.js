882b08296d904e051385b64c1231e9e8ac2c38e3
ansdb <anisdabdi0x@gmail.com>
2019-10-07 18:24:24 +0100

    I added a variable (arr) that holds quotes with their owners

---
+++ main.js
@@ -0,0 +1,43 @@
+/*https://youtu.be/wtV5ev6813I*/
+const quotes = [
+    {
+        "quote": "Ideas are the beginning points of all fortunes.",
+        "author": "Napoleon Hill",
+    },
+    {
+        "quote": "Don’t read success stories, you will only get a message. Read failure stories, you will get some ideas to get success.",
+        "author": "A. P. J. Abdul Kalam"
+    },
+    {
+        "quote": "If your ship doesn’t come in, swim out to it.",
+        "author": "Jonathan Winters"
+    },
+    {
+        "quote": "Quitters never win. Winners never quit.",
+        "author": "Dr. Irene C. Kassorla"
+    },
+    {
+        "quote": "Sadness flies away on the wings of time.",
+        "author": "Jean de la Fontaine"
+    },
+    {
+        "quote": "The true way to render ourselves happy is to love our work and find in it our pleasure.",
+        "author": "Francoise de Motteville"
+    },
+    {
+        "quote": "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        "author": "Thomas Edison"
+    },
+    {
+        "quote": "Patience is a virtue but you will never ever accomplish anything if you don't exercise action over patience.",
+        "author": "Byron Pulsifer"
+    },
+    {
+        "quote": "Kind words will unlock an iron door.",
+        "author": "Turkish proverb"
+    },
+    {
+        "quote": "I believe that every person is born with talent.",
+        "author": "Maya Angelou"
+    },
+];
