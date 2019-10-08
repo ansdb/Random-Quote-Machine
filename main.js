7d4bb857bb33a927b595882db0254208dc9052c7
Sapan Bodiwala <sapan.bodiwala@adp.com>
2019-10-07 20:31:58 -0400

    Added steve jobs quote

--- main.js
+++ main.js
@@ -58,6 +58,10 @@ const quotes = [
     {
         "quote":"The greatest glory in living lies not in never falling, but in rising every time we fall",
         "author": "Nelson Mandela"
+    },
+    {
+        "quote": "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma, which is living with the results of other people’s thinking.",
+        "author": "Steve Jobs"
     }
 ];
 
